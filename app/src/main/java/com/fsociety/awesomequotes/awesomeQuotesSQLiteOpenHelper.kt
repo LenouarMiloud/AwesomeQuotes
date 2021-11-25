5fd0c45d785a0b20f187fac8aac16dbd8f6a3d96
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-25 14:53:35 +0100

    add function to insert quotes category

--- app/src/main/java/com/fsociety/awesomequotes/awesomeQuotesSQLiteOpenHelper.kt
+++ app/src/main/java/com/fsociety/awesomequotes/awesomeQuotesSQLiteOpenHelper.kt
@@ -1,5 +1,6 @@
 package com.fsociety.awesomequotes
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -9,11 +10,27 @@ class awesomeQuotesSQLiteOpenHelper(context: Context) : SQLiteOpenHelper(context
     override fun onCreate(db: SQLiteDatabase?) {
         //create the quote table (quotes and categories)
         db!!.execSQL("CREATE TABLE quotes_categories(" +
-                "id INTEGER PRIMARY KEY" +
+                "_id INTEGER PRIMARY KEY" +
                 "picture_resource_id TEXT" +
                 "name TEXT" +
                 ")");
+
+        insertQuotesQuategory(db,1,R.drawable.alone,"Alone")
+        insertQuotesQuategory(db,2,R.drawable.alone,"Computer")
+        insertQuotesQuategory(db,3,R.drawable.alone,"Gamer")
+        insertQuotesQuategory(db,4,R.drawable.alone,"Matrix")
+
     }
+    //function to insert quote in the table
+    fun insertQuotesQuategory(db: SQLiteDatabase?,id:Int,ressourceId: Int,name:String){
+        val contentValue = ContentValues()
+        contentValue.put("_id",id)
+        contentValue.put("picture_resource_id",ressourceId)
+        contentValue.put("name",name)
+        db!!.insert("quotes_categories",null,contentValue)
+
+    }
+
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("Not yet implemented")
