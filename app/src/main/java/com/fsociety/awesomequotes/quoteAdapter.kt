1e3d0c8b5b998265279ff782286ba67b88ba2b60
LenouarMiloud <lenouarmiloud@gmail.com>
2021-11-30 11:57:29 +0100

    writing the Adapter Class

---
+++ app/src/main/java/com/fsociety/awesomequotes/quoteAdapter.kt
@@ -0,0 +1,40 @@
+package com.fsociety.awesomequotes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class quoteAdapter(val context : Context,val quotes:List<String>,val onItemLongClick:(String) -> Unit) : RecyclerView.Adapter<quoteAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.quote_item,parent,false)
+
+        return MyViewHolder(view, onItemLongClick)
+    }
+
+    override fun onBindViewHolder(viewHolder: MyViewHolder, quoteIndex: Int) {
+        viewHolder.bindQuote(quotes[quoteIndex])
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class MyViewHolder(itemView: View,onItemLongClick:(String) -> Unit): RecyclerView.ViewHolder(itemView){
+
+        val quoteText = itemView!!.findViewById<TextView>(R.id.textViewQuote)
+
+        fun bindQuote(quote:String){
+            quoteText.text = quote
+            itemView.setOnLongClickListener{
+                onItemLongClick(quote)
+                true
+            }
+        }
+
+    }
+
+}
